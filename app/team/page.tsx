d8ad4a0ed179229d117b571cc50bda4ed4a59792
IT24104332 <IT24104332@my.sliit.lk>
2026-01-30 18:44:00 +0530

    Update team member names in page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -9,9 +9,9 @@ import { Instagram } from "lucide-react";
 const team = [
     { name: "Rayon Fernando", role: "Creative Director", img: "/team/team-1.jpg" },
     { name: "Senuka Rosa", role: "Lead Photographer", img: "/team/team-2.jpg", link: "https://www.instagram.com/senuka.rosa/" },
-    { name: "Minusha Chalindu", role: "Videographer", img: "/team/team-3.jpg" },
+    { name: "Minusha Balasooriya", role: "Videographer", img: "/team/team-3.jpg" },
     { name: "Kanishka Lanza", role: "Drone Pilot", img: "/team/team-4.jpg" },
-    { name: "Shenal Nethmina", role: "Photographer", img: "/team/team-58.jpg" },
+    { name: "Shenal Fernando", role: "Photographer", img: "/team/team-58.jpg" },
     { name: "Thevindu Gunathilake", role: "Editor", img: "/team/team-6.jpg" },
 ];
 
