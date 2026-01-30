d81d2d052cb250e371fc1dc034f3d40dc16cf953
Sellappullage Senuka Dishara Rosa <IT24104385@my.sliit.lk>
2026-01-30 19:05:16 +0530

    Replace Google Analytics script with Google Tag Manager implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,24 +27,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google tag (gtag.js) */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-C3C4RZQ0C8"
-        ></Script>
-        <Script id="google-analytics">
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-C3C4RZQ0C8');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KB5J9JRW');
           `}
         </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={`${spaceGrotesk.variable} ${dmMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KB5J9JRW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
